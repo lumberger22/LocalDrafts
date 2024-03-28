81c2a5b684723d8aa42614df3f5f95fc336fc02b
lucasvt22 <lucas22@vt.edu>
2024-03-28 04:02:16 -0400

    Create brewery card layout

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-export default function Card() {
-
+export default function Card({ brewery }) {
 
     return (
         <>
-        
+            <div key={brewery.id} className='card'>
+                <a href={brewery.website_url} target='_blank' className="card--link">
+                    <h4 className="card--title">{brewery.name}</h4>
+                </a>
+                <p className="card--info">Type: {brewery.brewery_type}</p>
+                <p className="card--info">{brewery.street}, {brewery.city}, {brewery.state}</p>
+            </div>
         </>
     )
 }
