6534943eadfbeee18f0779839f077f3a6ee5c6d9
lucasvt22 <lucas22@vt.edu>
2024-03-28 04:03:28 -0400

    Reorg content section and add search and list features

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import Card from "./Components/Card.jsx"
 import Header from "./Components/Header.jsx"
-import List from "./Components/List.jsx"
+import Map from "./Components/Map.jsx"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [list, setList] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-  //Example API Calls
-  // https://api.openbrewerydb.org/v1/breweries?by_dist=32.88313237,-117.1649842&per_page=3
+  useEffect(() => {
+    const fetchBreweryData = async () => {
+      const response = await fetch(
+        `https://api.openbrewerydb.org/breweries?by_dist=${latitude},${longitude}&per_page=15`
+      );
+      const json = await response.json();
+      setList(json);
+      console.log(json);
+    };
+  
+    if (latitude && longitude) {
+      fetchBreweryData().catch(console.error);
+    }
+  }, [latitude, longitude]);
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -17,8 +33,8 @@ function App() {
   }
   
   function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
   }
 
@@ -26,17 +42,75 @@ function App() {
     console.log("Unable to retrieve your location");
   }
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = list.filter((brewery) =>
+        brewery?.name?.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(list);
+    }
+    console.log(filteredResults.length);
+  };
+
   return (
     <>
       <div className='whole--page'>
-        <Header/>
+        <Header />
         <div className='content--section'>
-          <div className='card--container'>
-            <Card/>
-            <Card/>
-            <Card/>
+          <div className='content--head'>
+            <div className='card--container'>
+              <div className='summary--card'>
+                <h1 className='summary-info'>
+                  {searchInput.length > 0 ? filteredResults.length : list.length}
+                </h1>
+                <h2 className='summary--label'>Breweries Shown</h2>
+              </div>
+              <div className='summary--card'>
+                <h1 className='summary-info'>
+                  {searchInput.length > 0 ? filteredResults.length : list.length}
+                </h1>
+                <h2 className='summary--label'>Breweries Shown</h2>
+              </div>
+            </div>
+            {latitude !== ""  && list.length > 0 ? (
+              <Map lat={latitude} list={list} filtered={filteredResults} long={longitude}/>
+            ): (
+              null
+            )} 
           </div>
-          <List/>
+                     
+          <div className='brewery--data'>
+            <h1>Breweries Near Me</h1>
+            <input
+              id='search--bar'
+              type="text"
+              placeholder="Search Name..."
+              onChange={(e) => searchItems(e.target.value)}
+            />
+            <hr />
+            <div className='brewery--container'>
+              {searchInput.length > 0 ? (
+                <>
+                  {filteredResults.length > 0 ? (
+                    filteredResults.map((brewery, index) => (
+                      <Card key={index} brewery={brewery} />
+                    ))
+                  ) : (
+                    <p>No results found.</p>
+                  )}
+                </>
+              ) : list.length > 0 ? (
+                list.map((brewery, index) => (
+                  <Card key={index} brewery={brewery} />
+                ))
+              ) : (
+                <p>Enable location accessibility to use Local Drafts</p>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
