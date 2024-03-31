dd538363725426e439154d1af6a404fe74288744
lucasvt22 <lucas22@vt.edu>
2024-03-30 21:46:59 -0400

    Create new files for Links and add BrowserRouter to main

---
+++ routes/DetailView.jsx
@@ -0,0 +1,12 @@
+import BreweryDetail from "../src/Components/BreweryDetail";
+
+const DetailView = () => {
+  return (
+    <div>
+      <Header />
+      <BreweryDetail />
+    </div>
+  );
+};
+
+export default DetailView;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailView from '/routes/DetailView';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+    <Route index={false} path="/breweryDetails/:name" element={<DetailView />} />
+        <Route index={true} element={<App />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+              <Link style={{ color: "white" }} to="/">
+                Back to Home
+              </Link>
+            </main>
+          }
+        />
+    </Routes>
+  </BrowserRouter>
 )
