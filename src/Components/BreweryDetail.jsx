6a0e29b1120d1e54259dbf85b3d9934e16a1b418
lucasvt22 <lucas22@vt.edu>
2024-04-01 01:13:28 -0400

    Change component to fetch single brewery based on id from path, add display

---
+++ src/Components/BreweryDetail.jsx
@@ -0,0 +1,35 @@
+import React, { Component, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function BreweryDetail() {
+
+    let params = useParams();
+    const [currentBrewery, setCurrentBrewery] = useState(null);
+
+    useEffect(() => {
+        const getBreweryDetail = async () => {
+          const description = await fetch(
+            `https://api.openbrewerydb.org/v1/breweries/${params.id}`
+          );
+
+          const descripJson = await description.json();
+          console.log(descripJson)
+          console.log(descripJson.name);
+          setCurrentBrewery(descripJson);
+        };
+    
+        getBreweryDetail().catch(console.error);
+      }, [params.id]);
+
+    return (
+        <>
+        {currentBrewery !== null ? (
+            <>
+                <div className="detail--section">
+                    <h1>{currentBrewery.name}</h1>
+                </div>
+            </>
+        ) : null}
+        </>
+    )
+}
