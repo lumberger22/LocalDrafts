780e11f3a0fbbe2b36aab5116ec9a08d169465f4
lucasvt22 <lucas22@vt.edu>
2024-04-01 01:15:17 -0400

    Add className to better align components

--- routes/DetailView.jsx
+++ routes/DetailView.jsx
@@ -1,8 +1,9 @@
 import BreweryDetail from "../src/Components/BreweryDetail";
+import Header from "../src/Components/Header";
 
 const DetailView = () => {
   return (
-    <div>
+    <div className="detail--page">
       <Header />
       <BreweryDetail />
     </div>
