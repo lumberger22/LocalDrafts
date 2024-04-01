b644852eba8789ed78a0cf8ad4f09f6d3a8f9653
lucasvt22 <lucas22@vt.edu>
2024-04-01 01:14:08 -0400

    Add conditional rendering of links and link detailView to main page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import Icon from "/beer-icon.png";
 
 export default function Header() {
 
@@ -6,19 +8,27 @@ export default function Header() {
         <>
             <div className="header">
                 <div className="header--title--container">
-                        <img className="header--title--icon" src="beer-icon.png" alt="Local Drafts Icon"/>
+                        <img className="header--title--icon" src={Icon} alt="Local Drafts Icon"/>
                         <h1 className="header--title">LocalDrafts</h1> 
                 </div>
                 <br/>
-                <a className='header--link' href="#title">
-                    <h2 className="header--option">Home</h2>
-                </a>
-                <a className='header--link' href="#about">
-                    <h2 className="header--option">About</h2>
-                </a>
-                <a className='header--link' href="#content">
-                    <h2 className="header--option">Dashboard</h2>
-                </a>
+                {window.location.pathname !== "/LocalDrafts/" ? (
+                    <Link className='header--link' to='/LocalDrafts/'>
+                        <h2 className="header--option">Go Back</h2>
+                    </Link>
+                ) : (
+                    <>
+                        <a className='header--link' href="#title">
+                            <h2 className="header--option">Home</h2>
+                        </a>
+                        <a className='header--link' href="#about">
+                            <h2 className="header--option">About</h2>
+                        </a>
+                        <a className='header--link' href="#content">
+                            <h2 className="header--option">Dashboard</h2>
+                        </a>
+                    </>
+                )}
             </div>            
         </>
     )
