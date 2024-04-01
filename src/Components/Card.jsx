74ad46d58ef3b075a38a96c8d65a9e0c6402ffde
lucasvt22 <lucas22@vt.edu>
2024-04-01 01:14:36 -0400

    Add links to detail views of each brewery to card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({ brewery }) {
 
     return (
         <>
             <div key={brewery.id} className='card'>
-                <a href={brewery.website_url} target='_blank' className="card--link">
+                <Link to={`/LocalDrafts/breweryDetails/${brewery.id}`} className="card--link">
                     <h4 className="card--title">{brewery.name}</h4>
-                </a>
+                </Link>
                 <p className="card--info">Type: {brewery.brewery_type}</p>
                 <p className="card--address">Address: {brewery.street}, {brewery.city}, {brewery.state}</p>
             </div>
